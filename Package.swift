8e8571609c7c9c0170e8012c2f0a75cfe04d3266
Meng Li <meng.li@monstar-lab.com>
2023-12-18 22:40:16 +0900

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "MGSelector",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "MGSelector", targets: ["MGSelector"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .target(name: "MGSelector", dependencies: ["SnapKit"], path: "MGSelector")
+    ],
+    swiftLanguageVersions: [.v5]
+)
